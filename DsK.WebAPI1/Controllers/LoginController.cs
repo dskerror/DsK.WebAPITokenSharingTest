1db72afaee1f6e41ab42fe8cd869db5bd9652f47
JonathanMartinezOptima <jonathan.martinez@optimapr.com>
2023-03-08 14:53:03 -0400

    Add project files.

---
+++ DsK.WebAPI1/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DsK.WebAPI1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            var token = await GenerateAuthenticationToken();
+            return Ok(token);
+        }
+
+        private async Task<string> GenerateAuthenticationToken()
+        {
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ThisIsTheKeyThisIsTheKeyThisIsTheKeyThisIsTheKey"));
+            var credentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            
+            var userClaims = new List<Claim>();
+            userClaims.Add(new Claim(ClaimTypes.Email, "email@domain.com"));
+            userClaims.Add(new Claim("UserId", "ThisIsAUserID"));
+            userClaims.Add(new Claim("UserName", "ThisIsAUserName"));
+            userClaims.Add(new Claim(ClaimTypes.Role, "ThisIsARole"));
+            
+
+            var newJwtToken = new JwtSecurityToken(
+                    issuer: "DsKIssuer",
+                    audience: "DsKAudience",
+                    expires: DateTime.UtcNow.AddDays(1),
+                    signingCredentials: credentials,
+                    claims: userClaims
+            );
+
+            string token = new JwtSecurityTokenHandler().WriteToken(newJwtToken);
+            return token;
+        }
+    }
+}
---
+++ DsK.WebAPI2/Program.cs
@@ -0,0 +1,93 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "WebAPI2",
+        Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+
+
+//builder.Services.Configure<TokenSettingsModel>(builder.Configuration.GetSection("TokenSettings"));
+
+//var IssuerSigningKey = builder.Configuration.GetSection("TokenSettings").GetValue<string>("Key") ?? "";
+//if (IssuerSigningKey == "")
+//{
+//    return; //Exit app if IssuerSigningKey is not found
+//}
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = "DsKIssuer", //builder.Configuration.GetSection("TokenSettings").GetValue<string>("Issuer"),
+        ValidateIssuer = true,
+        ValidAudience = "DsKAudience", //builder.Configuration.GetSection("TokenSettings").GetValue<string>("Audience"),
+        ValidateAudience = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ThisIsTheKeyThisIsTheKeyThisIsTheKeyThisIsTheKey")), //IssuerSigningKey
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+    };
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "myOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin();
+            builder.AllowAnyHeader();
+            builder.AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("myOrigins");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
